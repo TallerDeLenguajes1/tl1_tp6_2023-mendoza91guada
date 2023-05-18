55ac5e8435406c36df7b97b819b53dd68dadc744
Guadalupe <mendoza91guada@gmail.com>
2023-05-18 19:48:40 -0300

    Agrego .gitignore y más código

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -7,3 +7,21 @@ a=10;
 b=a;
 Console.WriteLine("Valor de a: "+a);
 Console.WriteLine("Valor de b: "+b);
+
+float num1=0;
+bool anda = false;
+string? numString = "";
+
+while(!anda)
+{
+    Console.WriteLine("Ingrese un numero valido: ");
+    numString=Console.ReadLine();
+    anda=float.TryParse(numString, out num1);
+    
+    if (!anda)
+    {
+        Console.WriteLine(numString + " no es numero valido");
+    } else {
+        Console.WriteLine("El numero ingresado es "+num1);
+    }
+}
