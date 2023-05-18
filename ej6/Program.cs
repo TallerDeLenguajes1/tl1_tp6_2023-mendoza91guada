18021360d333aff013d773161b682ceb3316dba1
Guadalupe <mendoza91guada@gmail.com>
2023-05-18 18:54:05 -0300

    Primer commit

---
+++ ej6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("El valor de a es: "+a);
+Console.WriteLine("El valor de b es: "+b);
